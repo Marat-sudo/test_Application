448c3c39650bdc1b1852888f6fd0ac56400f154e
Maratik <marat.ss.367@gmail.com>
2026-04-05 13:00:25 +0400

    пока работает

--- app/src/main/java/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,16 @@
 package ui.fragments
 
+
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+// Обязательно замените com.yourpackage на реальный ID вашего приложения!
+
+import android.os.Bundle
+import android.view.View
 import com.example.myapplication_2.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
@@ -8,6 +19,36 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     override fun onResume() {
         super.onResume()
 
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+
+        // 2. Добавляем провайдер меню
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Раздуваем ваше меню здесь
+                menuInflater.inflate(R.menu.settings_action_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Обрабатываем нажатия
+                return when (menuItem.itemId) {
+                    R.id.setting_menu_change_info -> true
+
+                    R.id.setting_menu_change_photo -> true
+
+                    R.id.setting_menu_exit -> true
+
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        // ^ Важно: передаем viewLifecycleOwner, чтобы меню само удалялось
     }
 
 }
